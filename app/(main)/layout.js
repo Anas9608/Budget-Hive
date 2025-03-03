adb834a017ad122884d95f6db4526f4f231dec65
Mohd Anas <anas0shah0@gmailcom>
2025-03-03 18:43:14 +0530

    minor adjustments in UI

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MainLayout = ({ children }) => {
   return (
-    <div className='flex justify-center pt-40' > {children} </div>
+    <div className='flex pt-40' > {children} </div>
   )
 }
 
