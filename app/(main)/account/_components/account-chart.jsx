e0f2ce287e62cf67ef492aa65c82c75847f12cd7
Mohd Anas <anas0shah0@gmailcom>
2025-04-18 21:18:42 +0530

    added delete transactions and related functionality

--- app/(main)/account/_components/account-chart.jsx
+++ app/(main)/account/_components/account-chart.jsx
@@ -112,8 +112,8 @@ export function AccountChart({ transactions }) {
             <p className="text-muted-foreground">Net</p>
             <p
               className={`text-lg font-bold ${totals.income - totals.expense >= 0
-                  ? "text-green-500"
-                  : "text-red-500"
+                ? "text-green-500"
+                : "text-red-500"
                 }`}
             >
               ${(totals.income - totals.expense).toFixed(2)}
